eb0fc80a475e61c7b44bc5a5b561257a4ae7caef
Chittesh-Kumar <128819733+ChitteshK139@users.noreply.github.com>
2024-04-15 13:42:12 +0530

    Mod_microservices-2

--- order_mgmt/app.py
+++ order_mgmt/app.py
@@ -23,31 +23,6 @@ def u_home():
     cur.close()
     return render_template('u_home.html', products=products)
 
-
-@app.route('/a_home')
-def a_home():
-    
-    # Check user session or authentication here before rendering the home page
-    cur = conn.cursor()
-    cur.execute("SELECT COUNT(*) FROM orders")
-    orders_count = cur.fetchone()['COUNT(*)']
-    cur.execute("SELECT COUNT(*) FROM users")
-    users_count = cur.fetchone()['COUNT(*)']
-    cur.execute("SELECT COUNT(*) FROM products")
-    products_count = cur.fetchone()['COUNT(*)']
-    cur.close()
-    
-    return render_template('a_home.html', orders_count=orders_count, users_count=users_count, products_count=products_count)
-
-@app.route('/orders')
-def orders():
-    # Fetch orders from the database
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM orders WHERE user_id=%s',int(session['user_id']))
-    orders = cursor.fetchall()
-    cursor.close()
-    return render_template('orders.html', orders=orders)
-
 @app.route('/orders_u')
 def orders_u():
     cursor = conn.cursor()
